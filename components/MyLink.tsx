669cdc11b9f2eda803c9946ca5ce14db00fa7bf0
7sne <kooblatum@gmail.com>
2022-03-08 15:38:41 +0100

    Fix eslint errors

--- components/MyLink.tsx
+++ components/MyLink.tsx
@@ -1,14 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
 
-interface MyLink {
+interface MyLinkProps {
   href: string
   as: string
   className?: string
   children: React.ReactNode
 }
 
-const MyLink = ({ className, href, as, children }: MyLink) => (
+const MyLink = ({ className, href, as, children }: MyLinkProps) => (
   <Link href={href} as={as} passHref>
     <a className={className || ''}>{children}</a>
   </Link>
