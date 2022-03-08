ad57fae3341faf66096eab5af9d3cf39cb3785b9
7sne <kooblatum@gmail.com>
2022-03-08 09:01:49 +0100

    Fix requested changes in ethUnitConversion

--- lib/ethUnitConversion.test.ts
+++ lib/ethUnitConversion.test.ts
@@ -1,20 +1,12 @@
 import { expect } from 'earljs'
 
-import { convertUnit, unitTypeToPrecision, unitPrecisionToValue } from './ethUnitConversion'
-
-describe('unitTypeToPrecision', () => {
-  it('converts named unit type to numerical', () => {
-    expect(unitTypeToPrecision('eth')).toEqual('18')
-    expect(unitTypeToPrecision('gwei')).toEqual('9')
-    expect(unitTypeToPrecision('wei')).toEqual('1')
-  })
-})
+import { convertUnit, unitPrecisionToValue } from './ethUnitConversion'
 
 describe('unitPrecisionToValue', () => {
   it('productes accurate values', () => {
-    expect(unitPrecisionToValue('1')).toEqual('1')
-    expect(unitPrecisionToValue('9')).toEqual('1000000000')
-    expect(unitPrecisionToValue('18')).toEqual('1000000000000000000')
+    expect(unitPrecisionToValue(1)).toEqual('1')
+    expect(unitPrecisionToValue(9)).toEqual('1000000000')
+    expect(unitPrecisionToValue(18)).toEqual('1000000000000000000')
   })
 })
 
@@ -40,11 +32,11 @@ describe('convertUnit', () => {
 
   it('performs lossy conversion ', () => {
     // @notice it fails, as currently we're not expecting decimals in such case
-    expect(convertUnit('0.00000412300033333300012', 'gwei', 'wei')).toEqual('4123') // !
+    expect(convertUnit('0.00000412300033333300012', 'gwei', 'wei')).toEqual('4123')
     expect(convertUnit('12', 'gwei', 'eth')).toEqual('0.000000000000004123')
   })
 
   it('performs untypical conversion', () => {
-    expect(convertUnit('0.12', 'wei', 'eth')).toEqual('0.00000000000000000012') // !
+    expect(convertUnit('0.12', 'wei', 'eth')).toEqual('0.00000000000000000012')
   })
 })
--- lib/ethUnitConversion.ts
+++ lib/ethUnitConversion.ts
@@ -4,39 +4,20 @@ BigNumber.set({ EXPONENTIAL_AT: 1000 })
 
 export type UnitType = 'wei' | 'gwei' | 'eth'
 
-// @todo handle hex value 0x.. input
-
 const unitPrecision: { [key in UnitType]: number } = {
   wei: 1,
   gwei: 9,
   eth: 18,
 }
 
-export function unitTypeToPrecision(unitType: UnitType): string | undefined {
-  switch (unitType) {
-    case 'eth':
-      return unitPrecision.eth.toString()
-    case 'gwei':
-      return unitPrecision.gwei.toString()
-    case 'wei':
-      return unitPrecision.wei.toString()
-    default:
-      break
-  }
-}
-
-export function unitPrecisionToValue(precision: string): string {
-  if (parseInt(precision) === 1) return '1'
-  return ['1'].concat('0'.repeat(parseInt(precision))).join('')
+export function unitPrecisionToValue(precision: number): string {
+  if (precision === 1) return '1'
+  return ['1'].concat('0'.repeat(precision)).join('')
 }
 
 export function convertUnit(value: string, fromUnit: UnitType, toUnit: UnitType): string | undefined {
-  const parsedFromUnit = unitTypeToPrecision(fromUnit)
-  const parsedToUnit = unitTypeToPrecision(toUnit)
-
-  if (parsedFromUnit && parsedToUnit) {
-    const toValue = unitPrecisionToValue(parsedFromUnit)
-    const fromValue = unitPrecisionToValue(parsedToUnit)
-    return new BigNumber(value).times(toValue).div(fromValue).toString()
-  }
+  return new BigNumber(value)
+    .times(unitPrecisionToValue(unitPrecision[fromUnit]))
+    .div(unitPrecisionToValue(unitPrecision[toUnit]))
+    .toString()
 }
