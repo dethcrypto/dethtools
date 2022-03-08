4901695d1cee3fa34f68fbecebb15c13a41ad79e
7sne <kooblatum@gmail.com>
2022-03-08 13:05:39 +0100

    Fix error in tokenUnitConversion

--- pages/dashboard/token-unit-conversion.tsx
+++ pages/dashboard/token-unit-conversion.tsx
@@ -1,36 +1,16 @@
-import { ChangeEvent, useState, Fragment } from 'react'
+import { ChangeEvent, Fragment, useState } from 'react'
 
 import { convertUnit, UnitType } from '../../lib/ethUnitConversion'
 
-export default function TokenUnitConversion() {
-  const [wei, setWei] = useState(0)
-  const [gwei, setGwei] = useState(0)
-  const [eth, setEth] = useState(0)
-
-  function updateValue(result: any) {
-    setWei(result.wei)
-    setGwei(result.gwei)
-    setEth(result.eth)
-  }
+export default function EthUnitConversion() {
+  const [wei, setWei] = useState('0')
+  const [gwei, setGwei] = useState('0')
+  const [eth, setEth] = useState('0')
 
   function resetValues() {
-    setWei(0)
-    setGwei(0)
-    setEth(0)
-  }
-
-  function handleChangeEvent(value: string, unitType: UnitType) {
-    const out = convertUnit(value, unitType)
-    if (!out) {
-      resetValues()
-    } else {
-      updateValue(out)
-    }
-  }
-
-  interface UnitTypeExtended {
-    name: UnitType
-    value: number
+    setWei('0')
+    setGwei('0')
+    setEth('0')
   }
 
   const units: UnitTypeExtended[] = [
@@ -39,36 +19,80 @@ export default function TokenUnitConversion() {
     { name: 'eth', value: eth },
   ]
 
-  function UnitElements(): JSX.Element {
-    return (
-      <Fragment>
-        {units.map((unit) => {
-          const { name, value } = unit
-          return (
-            <div className="flex flex-col gap-4 items-center">
-              <p className="text-lg"> {name} </p>
-              <input
-                placeholder={value ? value.toString() : '0'}
-                value={value}
-                type="number"
-                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                  console.log(event.target.value)
-                  handleChangeEvent(event.target.value, name)
-                }}
-                className="p-2 border border-dashed border-black"
-              />
-            </div>
-          )
-        })}
-      </Fragment>
-    )
+  function handleChangeEvent(value: string, unitType: UnitType) {
+    let out
+
+    for (const unit of units) {
+      out = convertUnit(value, unitType, unit.name)
+
+      if (out) {
+        if (unit.name === 'eth') setEth(out)
+        if (unit.name === 'gwei') setGwei(out)
+        if (unit.name === 'wei') setWei(out)
+      }
+    }
+
+    if (!out) {
+      resetValues()
+    }
   }
 
   return (
     <div className="flex ml-auto max-w-1/3 w-4/5 pl-24 mt-32">
       <form className="flex flex-col gap-10 mx-auto">
-        <UnitElements />
+        <h1> Token unit conversion </h1>
+        <UnitElements onChange={handleChangeEvent} units={units} />
       </form>
     </div>
   )
+}
+
+interface UnitElementsProps {
+  units: UnitTypeExtended[]
+  onChange: (value: string, unitType: UnitType) => void
+}
+function UnitElements({ units, onChange }: UnitElementsProps): JSX.Element {
+  return (
+    <Fragment>
+      {units.map((unit) => {
+        const { name, value } = unit
+        return (
+          <div>
+            <table className="min-w-full divide-y divide-gray-200 table-fixed">
+              <thead className="bg-gray-50 rounded-sm">
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-1 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                  >
+                    <p className="text-lg"> {name} </p>
+                  </th>
+
+                  <th
+                    scope="col"
+                    className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400"
+                  >
+                    <input
+                      placeholder={value ? value.toString() : '0'}
+                      value={value}
+                      type="number"
+                      onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                        onChange(event.target.value, name)
+                      }}
+                      className="p-3 w-72 border border-dashed border-black rounded-sm"
+                    />
+                  </th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+        )
+      })}
+    </Fragment>
+  )
+}
+
+interface UnitTypeExtended {
+  name: UnitType
+  value: string
 }
