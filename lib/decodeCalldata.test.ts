88e712d79a7914221d99b866d7f5cde4e303c684
7sne <kooblatum@gmail.com>
2022-03-15 10:31:54 +0100

    fix requested decode-calldata test changes

--- lib/decodeCalldata.test.ts
+++ lib/decodeCalldata.test.ts
@@ -8,14 +8,18 @@ describe(decodeCalldata.name, () => {
   it('handles expected case', () => {
     const iface = parseAbi('function transferFrom(address,address,uint256)')!
     expect(iface).toBeDefined()
+
     const decoded = decodeCalldata(
       iface,
       '0x23b872dd0000000000000000000000008ba1f109551bd432803012645ac136ddd64dba72000000000000000000000000ab7c8803962c0f2f5bbbe3fa8bf41cd82aa1923c0000000000000000000000000000000000000000000000000de0b6b3a7640000',
     )!
+    expect(decoded.fragment.format()).toEqual('transferFrom(address,address,uint256)')
+
     expect(decoded.decoded[0]).toEqual('0x8ba1f109551bD432803012645Ac136ddd64DBA72')
     expect(decoded.decoded[1]).toEqual('0xaB7C8803962c0f2F5BBBe3FA8bf41cd82AA1923C')
-    const bn = decoded.decoded[2] as BigNumber
-    expect(bn._hex).toEqual('0x0de0b6b3a7640000')
+
+    expect((decoded.decoded[2] as BigNumber).toString()).toEqual('1000000000000000000')
+
     expect(decoded.fragment).toBeDefined()
   })
 
@@ -25,20 +29,25 @@ describe(decodeCalldata.name, () => {
       'function transferFrom(address from, address to, uint256 amount)',
       'function getUser(uint256 id) view returns (tuple(string name, address addr) user)',`)!
     expect(iface).toBeDefined()
+
     const decoded = decodeCalldata(
       iface,
       '0x23b872dd0000000000000000000000008ba1f109551bd432803012645ac136ddd64dba72000000000000000000000000ab7c8803962c0f2f5bbbe3fa8bf41cd82aa1923c0000000000000000000000000000000000000000000000000de0b6b3a7640000',
     )!
+    expect(decoded.fragment.format()).toEqual('transferFrom(address,address,uint256)')
+
     expect(decoded.decoded[0]).toEqual('0x8ba1f109551bD432803012645Ac136ddd64DBA72')
     expect(decoded.decoded[1]).toEqual('0xaB7C8803962c0f2F5BBBe3FA8bf41cd82AA1923C')
-    const bn = decoded.decoded[2] as BigNumber
-    expect(bn._hex).toEqual('0x0de0b6b3a7640000')
+
+    expect((decoded.decoded[2] as BigNumber).toString()).toEqual('1000000000000000000')
+
     expect(decoded.fragment).toBeDefined()
   })
 
   it('handles expected case, but calldata is not found', () => {
     const iface = parseAbi('function transferFrom(address,address,uint256)')!
     expect(iface).toBeDefined()
+
     expect(decodeCalldata(iface, '0x0')).not.toBeDefined()
   })
 })
