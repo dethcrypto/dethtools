07a43905d702819058843846c79f03d24df3844d
7sne <kooblatum@gmail.com>
2022-07-07 16:51:49 +0200

    Add `parseEthersErrorMessage` description

--- src/misc/parseEthersErrorMessage.ts
+++ src/misc/parseEthersErrorMessage.ts
@@ -1,3 +1,5 @@
+// Parses ethers error which gives short error description before details
+// in the ...x(...y) format. We want to pick x part only.
 export function parseEthersErrorMessage(errorMsg: string): string {
   let error: string = '';
   for (const word of errorMsg) {
