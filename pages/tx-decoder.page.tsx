dda79a3d9a7b3edda821926f646d4e74d84f5040
7sne <kooblatum@gmail.com>
2022-07-07 16:20:58 +0200

    Make tx decoder errors better

--- pages/tx-decoder.page.tsx
+++ pages/tx-decoder.page.tsx
@@ -1,32 +1,70 @@
 import { TypedTransaction } from '@ethereumjs/tx';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
+import { hexSchema } from '../src/misc/schemas/hexSchema';
+import { WithOkAndErrorMsgOptional } from '../src/misc/types';
 
 import { DecodersIcon } from '../src/components/icons/DecodersIcon';
 import { Button } from '../src/components/lib/Button';
 import { ToolContainer } from '../src/components/ToolContainer';
 import { ToolHeader } from '../src/components/ToolHeader';
 import { DecodedTx, decodeTx } from '../src/lib/decodeTx';
+import { zodResultMessage } from '../src/misc/zodResultMessage';
 import { toEvenHex } from '../src/lib/toEvenHex';
 
 export default function TxDecoder() {
   const [error, setError] = useState<string>();
 
-  const [rawTx, setRawTx] = useState<string>();
+  const [rawTx, setRawTx] = useState<WithOkAndErrorMsgOptional<string>>({
+    isOk: true,
+  });
   const [decodeResults, setDecodeResults] =
     useState<{ tx: TypedTransaction; senderAddr: string }>();
 
-  function handleDecodeCalldata() {
-    setDecodeResults(undefined);
-    let decoded: DecodedTx | undefined;
-    try {
-      if (rawTx) decoded = decodeTx(rawTx);
-    } catch (e) {
-      setError('Unable to decode transaction');
+  function handleChangeRawTx(event: ChangeEvent<HTMLInputElement>) {
+    let { value } = event.target;
+    value = toEvenHex(value);
+    setRawTx((state) => {
+      return { ...state, inner: value };
+    });
+    const parseResult = hexSchema.safeParse(value);
+    if (parseResult.success) {
+      setRawTx((state) => {
+        return { ...state, isOk: true, errorMsg: undefined };
+      });
+    } else {
+      setRawTx((state) => {
+        return {
+          ...state,
+          isOk: false,
+          errorMsg: zodResultMessage(parseResult),
+        };
+      });
     }
-    if (decoded) setDecodeResults(decoded);
+    if (value.length === 0) {
+      setRawTx((state) => {
+        return {
+          ...state,
+          inner: value,
+          isOk: true,
+          errorMsg: undefined,
+        };
+      });
+    }
   }
 
-  const decodeButtonDisabled = !rawTx;
+  function handleDecodeCalldata() {
+    setDecodeResults(undefined);
+    if (rawTx.inner) {
+      let decoded: DecodedTx | undefined;
+      try {
+        decoded = decodeTx(rawTx.inner);
+      } catch (e) {
+        setError('Unable to decode transaction');
+      }
+      if (decoded) setDecodeResults(decoded);
+    }
+  }
+  const decodeButtonDisabled = !rawTx.inner;
 
   return (
     <ToolContainer>
@@ -34,21 +72,26 @@ export default function TxDecoder() {
         icon={<DecodersIcon height={24} width={24} />}
         text={['Decoders', 'Tx Decoder']}
       />
-
       <section>
         <label htmlFor="tx-input">raw transaction</label>
-
-        <input
-          id="tx-input"
-          type="text"
-          placeholder="e.g 0x0..."
-          className="mb-2 mt-1 mr-auto h-10 w-full rounded-md border border-gray-600 bg-gray-900 text-sm focus:outline-none"
-          onChange={(event) => setRawTx(toEvenHex(event.target.value))}
-        />
+        <>
+          <input
+            id="tx-input"
+            type="text"
+            placeholder="e.g 0x0..."
+            className={
+              'mb-2 mt-1 mr-auto h-10 w-full rounded-md border' +
+              '  bg-gray-900 text-sm focus:outline-none' +
+              String(rawTx.isOk ? ' border-gray-600' : ' border-error')
+            }
+            onChange={(event) => handleChangeRawTx(event)}
+          />
+          <p className="text-right text-error">{rawTx.errorMsg}</p>
+        </>
       </section>
 
       <Button
-        onClick={() => void handleDecodeCalldata()}
+        onClick={() => handleDecodeCalldata()}
         className="mt-4"
         disabled={decodeButtonDisabled}
         title={decodeButtonDisabled ? 'Please fill in the tx field' : undefined}
@@ -58,7 +101,7 @@ export default function TxDecoder() {
 
       <section className="pt-8 pb-3">
         {decodeResults ? (
-          rawTx ? (
+          rawTx.inner ? (
             <p className="text-md pb-4 font-semibold">
               Possible decoded results:
             </p>
@@ -76,7 +119,7 @@ export default function TxDecoder() {
       >
         {error ? (
           <p className="text-error">
-            {error} with {rawTx}
+            {error} with {rawTx.inner}
           </p>
         ) : (
           <output>
