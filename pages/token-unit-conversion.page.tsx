1b3c8152d8d553f5e56ded2fe8f951ba27b1afb4
7sne <kooblatum@gmail.com>
2022-07-20 13:04:45 +0200

    Add predefined decimals to `token-unit-conversion` component

--- pages/token-unit-conversion.page.tsx
+++ pages/token-unit-conversion.page.tsx
@@ -16,6 +16,7 @@ import { convertUnit } from '../src/lib/convertUnits';
 import { decodeHex } from '../src/lib/decodeHex';
 import { unitSchema } from '../src/misc/schemas/unitSchema';
 import { WithError } from '../src/misc/types';
+import { CurrencyIcon } from '../src/components/icons/Currency';
 
 const DEFAULT_DECIMALS = 18;
 
@@ -57,9 +58,7 @@ export default function TokenUnitConversion(): ReactElement {
         // return here to not override error set above
         return;
       }
-
       const parsed = unitSchema.safeParse(newValue);
-
       if (!parsed.success) {
         setState((prevState) => ({
           ...prevState,
@@ -68,7 +67,6 @@ export default function TokenUnitConversion(): ReactElement {
             error: parsed.error.errors[0].message,
           },
         }));
-
         return;
       }
 
@@ -79,21 +77,15 @@ export default function TokenUnitConversion(): ReactElement {
           ...oldState,
           [currentType]: { value: newValue },
         };
-
         if (decimals.error) return newState;
-
         const otherUnit = currentType === 'base' ? 'unit' : 'base';
-
         const out = convertUnit(newValue, currentType, otherUnit, {
           unit: 1,
           base: decimals.value + 1,
         });
-
         if (out !== undefined && !isNaN(parseInt(out)))
           newState[otherUnit] = { value: out };
-
         lastUpdate.current = { name: currentType, value: newValue };
-
         return newState;
       });
     };
@@ -131,29 +123,73 @@ interface DecimalsInputProps {
   setDecimals: Dispatch<SetStateAction<WithError<number>>>;
 }
 
+interface PredefinedDecimal {
+  name: string;
+  value: number;
+}
+
+const predefinedDecimals: PredefinedDecimal[] = [
+  { name: 'gwei', value: 15 },
+  { name: 'wad', value: 18 },
+  { name: 'ray', value: 27 },
+  { name: 'rad', value: 45 },
+];
+
 function DecimalsInput({
   decimals,
   setDecimals,
 }: DecimalsInputProps): ReactElement {
+  const [showPredefined, setShowPredefined] = useState(false);
   return (
-    <ConversionInput
-      name="Decimals"
-      value={decimals.value}
-      error={decimals.error}
-      type="number"
-      min={0}
-      max={26}
-      onChange={(e) => {
-        let error = e.target.validationMessage;
-
-        const value = e.target.valueAsNumber;
-        if (isNaN(value) || value < 0) {
-          error = 'The decimals must be a number between 0 and 26';
-        }
-
-        setDecimals({ value, error });
-      }}
-    />
+    <div className="relative">
+      <ConversionInput
+        className="w-full pr-10"
+        name="Decimals"
+        value={decimals.value}
+        error={decimals.error}
+        type="number"
+        onChange={(e) => {
+          const value = e.target.valueAsNumber;
+          setDecimals({ value });
+        }}
+      />
+      <div
+        aria-label="predefined decimals button"
+        className="absolute top-10 right-4 cursor-pointer duration-200 hover:scale-105 active:scale-90"
+        onClick={() => setShowPredefined(!showPredefined)}
+      >
+        <CurrencyIcon width={18} height={18} color="#0000000" />
+      </div>
+      <section
+        aria-label="predefined decimals dropdown"
+        className={`absolute w-full rounded-md border border-gray-600 bg-gray-800 px-4 py-2 shadow-md ${
+          showPredefined ? 'opacity-1 block select-none' : 'hidden opacity-0'
+        }`}
+      >
+        {predefinedDecimals.map((decimal) => {
+          return (
+            <div
+              aria-label={`${decimal.name} predefined decimal`}
+              key={decimal.name}
+              onClick={() => {
+                setDecimals({ value: decimal.value });
+                setShowPredefined(!showPredefined);
+              }}
+              className="flex cursor-pointer rounded-md p-1 duration-200 
+                       hover:bg-gray-600 active:scale-95"
+            >
+              <p aria-label="predefined decimal name">{decimal.name}</p>
+              <sup
+                aria-label="predefined decimal value"
+                className="inline-block text-sm leading-none text-gray-300"
+              >
+                {decimal.value}
+              </sup>
+            </div>
+          );
+        })}
+      </section>
+    </div>
   );
 }
 
