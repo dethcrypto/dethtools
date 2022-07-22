322a094d01b04f4c468e58e00e10efd2d4c35af2
7sne <kooblatum@gmail.com>
2022-07-22 16:46:45 +0200

    Change `decodeBySigHash` structure

--- src/lib/decodeBySigHash.ts
+++ src/lib/decodeBySigHash.ts
@@ -1,36 +1,34 @@
 import { Interface } from '@ethersproject/abi';
 import fetch from 'node-fetch';
-
 import { hexSchema } from '../misc/schemas/hexSchema';
 import { decodeCalldata, DecodeResult } from './decodeCalldata';
 import { DecodedEventResult, decodeEvent, EventProps } from './decodeEvent';
 import { parseAbi } from './parseAbi';
 
-export async function decodeWithEventProps(
-  sigHash: string,
-  eventProps: EventProps,
-): Promise<DecodedEventResult[] | undefined> {
-  const data = await getSignaturesByTopic(sigHash);
-  if (data) {
-    // force indexing basing on topic count
-    const ifaces = parse4BytesResToIfaces(data, 'event');
-    return decode4BytesData(ifaces, eventProps, decodeEvent);
-  }
-}
-
-export async function fetchAndDecodeWithCalldata(
+export async function decodeWithCalldata(
   sigHash: string,
   calldata: string,
 ): Promise<DecodeResult[] | undefined> {
-  const data = await getSignaturesByCalldata(sigHash);
-  if (data) {
-    const ifaces = parse4BytesResToIfaces(data);
+  const response = await fetch4BytesBy.Signatures(sigHash);
+  if (response) {
+    const ifaces = parse4BytesResToIfaces(response);
     const decodedByCalldata = decodeByCalldata(ifaces, calldata);
     if (decodedByCalldata.length === 0 && ifaces.length > 0) {
       return [{ decoded: [], fragment: ifaces[0].fragments[0], sigHash }];
     } else {
       return decodedByCalldata;
     }
+  }
+}
+
+export async function decodeWithEventProps(
+  sigHash: string,
+  eventProps: EventProps,
+): Promise<DecodedEventResult[] | undefined> {
+  const response = await fetch4BytesBy.EventProps(sigHash);
+  if (response) {
+    const ifaces = parse4BytesResToIfaces(response, 'event');
+    return decode4BytesData(ifaces, eventProps, decodeEvent);
   }
 }
 
@@ -41,52 +39,28 @@ export function sigHashFromCalldata(calldata: string): string | undefined {
   }
 }
 
-export type FetchResult = {
-  id: number;
-  text_signature: string;
-  bytes_signature: string;
-  created_at: string;
-  hex_signature: string;
+export const fetch4BytesBy = {
+  EventProps: async (sigHash: string) => {
+    return fetch4Bytes(sigHash, 'event-signatures');
+  },
+  Signatures: async (sigHash: string) => {
+    return fetch4Bytes(sigHash, 'signatures');
+  },
 };
+
+async function fetch4Bytes(
+  hexSig: string,
+  hexSigType: HexSigType,
+): Promise<FourBytes[] | undefined> {
+  const { results } = await safeFetch<FourBytesResponse>(
+    `${urlTo(hexSigType)}${hexSig}`,
+  );
+  return results;
+}
 
 // @internal
 // there are more types, but we don't need them for now
 export type HexSigType = 'signatures' | 'event-signatures';
-
-type Bytes4Cache = {
-  [sigType in HexSigType]: {
-    // undefined - not populated
-    // [] - no results
-    // [...] - results
-    [sig: string]: FetchResult[] | undefined;
-  };
-};
-
-const bytes4Cache: Bytes4Cache = {
-  signatures: {},
-  'event-signatures': {},
-};
-
-// @internal
-export async function getBytes4Data(
-  hexSig: string,
-  hexSigType: HexSigType,
-): Promise<FetchResult[] | undefined> {
-  if (bytes4Cache[hexSigType][hexSig] === undefined) {
-    await fetch4BytesData(hexSig, hexSigType);
-  }
-  return bytes4Cache[hexSigType][hexSig];
-}
-
-// @internal
-export async function fetch4BytesData(
-  hexSig: string,
-  hexSigType: HexSigType,
-): Promise<FetchResult[] | undefined> {
-  const response = await fetch(`${urlTo(hexSigType)}${hexSig}`);
-  const json = await response.json();
-  return (bytes4Cache[hexSigType][hexSig] = json.results);
-}
 
 // @internal
 function urlTo(hexSigType: HexSigType): string {
@@ -94,29 +68,13 @@ function urlTo(hexSigType: HexSigType): string {
 }
 
 // @internal
-export async function getSignaturesByCalldata(
-  sigHash: string,
-): Promise<FetchResult[] | undefined> {
-  return getBytes4Data(sigHash, 'signatures');
-}
-
-// @internal
-export async function getSignaturesByTopic(
-  sigHash: string,
-): Promise<FetchResult[] | undefined> {
-  return getBytes4Data(sigHash, 'event-signatures');
-}
-
-// @internal
 export function parse4BytesResToIfaces(
-  data: FetchResult[],
+  data: FourBytes[],
   defaultKeyword: string = 'function',
 ): Interface[] {
   const ifaces: Interface[] = [];
-
   for (const result of data) {
     const frag = result.text_signature;
-
     let parsed: Interface | Error;
     try {
       parsed = parseAbi(frag, defaultKeyword);
@@ -124,6 +82,31 @@ export function parse4BytesResToIfaces(
     } catch (e) {}
   }
   return ifaces;
+}
+
+async function safeFetch<T>(...args: Parameters<typeof fetch>): Promise<T> {
+  return fetch(...args).then(async (response) => {
+    if (response.status === 200) {
+      return response.json() as unknown as T;
+    } else {
+      throw new Error(`${response.status} ${response.statusText}`);
+    }
+  });
+}
+
+interface FourBytes {
+  id: number;
+  text_signature: string;
+  bytes_signature: string;
+  created_at: string;
+  hex_signature: string;
+}
+
+interface FourBytesResponse {
+  count: number;
+  next: unknown;
+  previous: unknown;
+  results: FourBytes[];
 }
 
 // @internal
