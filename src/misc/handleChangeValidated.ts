9dee1cca3a928d80d18e892d0170d155685b8a79
7sne <kooblatum@gmail.com>
2022-08-03 12:10:05 +0200

    Add helper function for common change -> validate pattern

--- src/misc/handleChangeValidated.ts
+++ src/misc/handleChangeValidated.ts
@@ -1,29 +1,42 @@
-import { WithError } from './types';
+import { ValidatorResult } from './validation/validators/result.d';
 
+/**
+ * @param newValue
+ * @param setState
+ * @param validateFn - function that validates the new value 
+    (preferably function following validator convention)
+    @see src/misc/validation/validators
+ * @param flushFn - function to flush the component state (e.g results)
+ * @param args - additional arguments to pass to setState function
+ * @returns {ValidatorResult}
+ */
 export function handleChangeValidated({
   newValue,
   setState,
   validateFn,
   flushFn,
-}: HandleChangeValidatedArgs): void {
+  ...args
+}: HandleChangeValidatedArgs & AnyArgs): void {
   // flush component results
   flushFn();
   // parse with provided schema
   const validationResult = validateFn(newValue);
 
   setState({
+    ...args,
     value: newValue,
     error: !validationResult.success ? validationResult.error : undefined,
   });
   // clear error if field is empty now
-  if (newValue.length === 0) setState({ value: newValue, error: undefined });
+  if (newValue.length === 0)
+    setState({ ...args, value: newValue, error: undefined });
 }
 
 interface HandleChangeValidatedArgs {
   newValue: string;
-  validateFn: (
-    newValue: string,
-  ) => { success: true; error: undefined } | { success: false; error: string };
-  setState: (value: WithError<string>) => void;
+  validateFn: (newValue: string) => ValidatorResult;
+  setState: (...args: any[]) => void;
   flushFn: () => void;
 }
+
+type AnyArgs = { [key: string]: any };
