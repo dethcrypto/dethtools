be8306483358b90421eb13819f46b4a004fb1c0a
7sne <kooblatum@gmail.com>
2022-08-03 12:11:01 +0200

    Simplify event-decoder and its tests

--- pages/event-decoder/event-decoder.test.tsx
+++ pages/event-decoder/event-decoder.test.tsx
@@ -1,6 +1,7 @@
-import { fireEvent, render, waitFor } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import { expect } from 'earljs';
 import sinon from 'sinon';
+import { changeTargetValue } from '../../test/helpers/changeTargetValue';
 
 import { fetch4BytesBy } from '../../src/lib/decodeBySigHash';
 import EventDecoder from './index.page';
@@ -18,16 +19,15 @@ describe(EventDecoder.name, () => {
       'text area for abi',
     )) as HTMLTextAreaElement;
 
-    fireEvent.change(abiField, {
-      target: {
-        value: `
+    changeTargetValue(
+      abiField,
+      `
       'constructor(string symbol, string name)',
       'function transferFrom(address from, address to, uint256 amount)',
       'function getUser(uint256 id) view returns (tuple(string name, address addr) user)',
       'event Transfer(address indexed, address indexed, uint256)',
       `,
-      },
-    });
+    );
 
     expect(abiField.value).toEqual(`
       'constructor(string symbol, string name)',
@@ -47,42 +47,21 @@ describe(EventDecoder.name, () => {
       'topic2',
     )) as HTMLInputElement;
 
-    fireEvent.change(dataField, {
-      target: {
-        value:
-          '0x0000000000000000000000000000000000000000000000000de0b6b3a7640000',
-      },
-    });
-    fireEvent.change(topic0Field, {
-      target: {
-        value:
-          '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
-      },
-    });
-    fireEvent.change(topic1Field, {
-      target: {
-        value:
-          '0x0000000000000000000000008ba1f109551bd432803012645ac136ddd64dba72',
-      },
-    });
-    fireEvent.change(topic2Field, {
-      target: {
-        value:
-          '0x000000000000000000000000ab7c8803962c0f2f5bbbe3fa8bf41cd82aa1923c',
-      },
-    });
-
-    expect(dataField.value).toEqual(
-      '0x0000000000000000000000000000000000000000000000000de0b6b3a7640000',
-    );
-    expect(topic0Field.value).toEqual(
+    changeTargetValue(
+      topic0Field,
       '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
     );
-    expect(topic1Field.value).toEqual(
+    changeTargetValue(
+      topic1Field,
       '0x0000000000000000000000008ba1f109551bd432803012645ac136ddd64dba72',
     );
-    expect(topic2Field.value).toEqual(
+    changeTargetValue(
+      topic2Field,
       '0x000000000000000000000000ab7c8803962c0f2f5bbbe3fa8bf41cd82aa1923c',
+    );
+    changeTargetValue(
+      dataField,
+      '0x0000000000000000000000000000000000000000000000000de0b6b3a7640000',
     );
 
     const decodeButton = (await root.findByText('Decode')) as HTMLButtonElement;
@@ -121,147 +100,44 @@ describe(EventDecoder.name, () => {
 
     const root = render(<EventDecoder />);
 
-    const dataField = (await root.findByLabelText('data')) as HTMLInputElement;
-    const topic0Field = (await root.findByLabelText(
-      'topic0',
-    )) as HTMLInputElement;
-    const topic1Field = (await root.findByLabelText(
-      'topic1',
-    )) as HTMLInputElement;
-    const topic2Field = (await root.findByLabelText(
-      'topic2',
-    )) as HTMLInputElement;
+    const dataField = root.getByLabelText('data') as HTMLInputElement;
+    const topic0Field = root.getByLabelText('topic0') as HTMLInputElement;
+    const topic1Field = root.getByLabelText('topic1') as HTMLInputElement;
+    const topic2Field = root.getByLabelText('topic2') as HTMLInputElement;
 
-    fireEvent.change(dataField, {
-      target: {
-        value:
-          '0x0000000000000000000000000000000000000000000000000000000000000000',
-      },
-    });
-    fireEvent.change(topic0Field, {
-      target: {
-        value:
-          '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925',
-      },
-    });
-    fireEvent.change(topic1Field, {
-      target: {
-        value:
-          '0x0000000000000000000000005853ed4f26a3fcea565b3fbc698bb19cdf6deb85',
-      },
-    });
-    fireEvent.change(topic2Field, {
-      target: {
-        value:
-          '0x000000000000000000000000e1be5d3f34e89de342ee97e6e90d405884da6c67',
-      },
-    });
-
-    expect(dataField.value).toEqual(
-      '0x0000000000000000000000000000000000000000000000000000000000000000',
-    );
-    expect(topic0Field.value).toEqual(
+    changeTargetValue(
+      topic0Field,
       '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925',
     );
-    expect(topic1Field.value).toEqual(
+    changeTargetValue(
+      topic1Field,
       '0x0000000000000000000000005853ed4f26a3fcea565b3fbc698bb19cdf6deb85',
     );
-    expect(topic2Field.value).toEqual(
+    changeTargetValue(
+      topic2Field,
       '0x000000000000000000000000e1be5d3f34e89de342ee97e6e90d405884da6c67',
     );
+    changeTargetValue(
+      dataField,
+      '0x0000000000000000000000000000000000000000000000000000000000000000',
+    );
 
-    const decodeButton = (await root.findByText('Decode')) as HTMLButtonElement;
+    const decodeButton = root.getByText('Decode') as HTMLButtonElement;
 
     fireEvent.click(decodeButton);
 
-    await waitFor(() =>
-      expect(root.findAllByLabelText('decoded value')).toBeDefined(),
+    const correctDecodedValues = [
+      '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925', // signature hash
+      '0x5853eD4f26A3fceA565b3FBC698bb19cdF6DEB85',
+      '0xE1Be5D3f34e89dE342Ee97E6e90D405884dA6c67',
+      '0',
+    ];
+    const decodedValues = (await root.findAllByLabelText('decoded value')).map(
+      (decoded) => decoded.innerHTML,
     );
-
-    let args: HTMLElement[] = [];
-
-    await waitFor(async () => {
-      args = await root.findAllByLabelText('decoded event arg index');
+    decodedValues.forEach((decoded, i) => {
+      expect(correctDecodedValues[i]).toEqual(decoded);
     });
-
-    const arg0 = args[0];
-    const arg1 = args[1];
-    const arg2 = args[2];
-
-    expect(arg0.parentElement!.innerHTML).toEqual(
-      expect.stringMatching('0x5853eD4f26A3fceA565b3FBC698bb19cdF6DEB85'),
-    );
-    expect(arg1.parentElement!.innerHTML).toEqual(
-      expect.stringMatching('0xE1Be5D3f34e89dE342Ee97E6e90D405884dA6c67'),
-    );
-    expect(arg2.parentElement!.innerHTML).toEqual(expect.stringMatching('0'));
-  });
-
-  it.skip('works with non-0x prefixed values', async () => {
-    const root = render(<EventDecoder />);
-
-    fireEvent.click(root.getByText('4 bytes'));
-
-    const dataField = (await root.findByLabelText('data')) as HTMLInputElement;
-    const topic0Field = (await root.findByLabelText(
-      'topic0',
-    )) as HTMLInputElement;
-    const topic1Field = (await root.findByLabelText(
-      'topic1',
-    )) as HTMLInputElement;
-    const topic2Field = (await root.findByLabelText(
-      'topic2',
-    )) as HTMLInputElement;
-
-    fireEvent.change(dataField, {
-      target: {
-        value:
-          '0000000000000000000000000000000000000000000000000000000000000000',
-      },
-    });
-    fireEvent.change(topic0Field, {
-      target: {
-        value:
-          '8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925',
-      },
-    });
-    fireEvent.change(topic1Field, {
-      target: {
-        value:
-          '0000000000000000000000005853ed4f26a3fcea565b3fbc698bb19cdf6deb85',
-      },
-    });
-    fireEvent.change(topic2Field, {
-      target: {
-        value:
-          '000000000000000000000000e1be5d3f34e89de342ee97e6e90d405884da6c67',
-      },
-    });
-
-    const decodeButton = (await root.findByText('Decode')) as HTMLButtonElement;
-
-    fireEvent.click(decodeButton);
-
-    expect(
-      await root.findByText(
-        '0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925',
-      ),
-    );
-
-    const arg0 = await waitFor(() => root.findByText('"0"'));
-    const arg1 = await waitFor(() => root.findByText('"1"'));
-    const arg2 = await waitFor(() => root.findByText('"2"'));
-
-    expect(arg0.parentElement!.innerHTML).toEqual(
-      expect.stringMatching(/0x5853eD4f26A3fceA565b3FBC698bb19cdF6DEB85/),
-    );
-    expect(arg1.parentElement!.innerHTML).toEqual(
-      expect.stringMatching(/0xE1Be5D3f34e89dE342Ee97E6e90D405884dA6c67/),
-    );
-    expect(arg2.parentElement!.innerHTML).toEqual(expect.stringMatching(/0/));
-    expect(arg0.parentElement!.innerHTML).toMatchSnapshot();
-    expect(arg1.parentElement!.innerHTML).toMatchSnapshot();
-    expect(arg2.parentElement!.innerHTML).toMatchSnapshot();
   });
 
   it('types topic in wrong format, gets error message', async () => {
--- pages/event-decoder/index.page.tsx
+++ pages/event-decoder/index.page.tsx
@@ -1,5 +1,4 @@
 import { Interface } from '@ethersproject/abi';
-import { addHexPrefix } from 'ethereumjs-util';
 import {
   ChangeEvent,
   ClipboardEvent,
@@ -7,6 +6,7 @@ import {
   useMemo,
   useState,
 } from 'react';
+import { handleChangeValidated } from '../../src/misc/handleChangeValidated';
 
 import { AbiSourceTabs } from '../../src/components/AbiSourceTabs';
 import { CopyableConversionInput } from '../../src/components/CopyableConversionInput';
@@ -27,298 +27,165 @@ import {
 } from '../../src/lib/decodeEvent';
 import { parseAbi } from '../../src/lib/parseAbi';
 import { parseEthersErrorMessage } from '../../src/misc/parseEthersErrorMessage';
-import { hexSchema } from '../../src/misc/schemas/hexSchema';
-import {
-  WithOkAndErrorMsg,
-  WithOkAndErrorMsgOptional,
-} from '../../src/misc/types';
-import { zodResultMessage } from '../../src/misc/zodResultMessage';
+import { WithError } from '../../src/misc/types';
+import { hexValidator } from '../../src/misc/validation/validators/hexValidator';
+import { abiValidator } from '../../src/misc/validation/validators/abiValidator';
 
-interface HandleChangeTopic {
-  index: number;
-  event: ChangeEvent<HTMLInputElement>;
-  isPasted: false;
-}
+const stateBeforeIndex = (
+  state: WithError<string>[],
+  index: number,
+): WithError<string>[] => {
+  return [...state.slice(0, index)];
+};
 
-interface HandlePasteTopic {
-  index: number;
-  event: ClipboardEvent<HTMLInputElement>;
-  isPasted: true;
-}
+const stateAfterIndex = (
+  state: WithError<string>[],
+  index: number,
+): WithError<string>[] => {
+  return [...state.slice(index + 1)];
+};
 
 export default function EventDecoder(): ReactElement {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string>();
   const [tab, setTab] = useState<'abi' | '4-bytes'>('4-bytes');
-  const [rawAbi, setRawAbi] = useState<WithOkAndErrorMsgOptional<string>>({
-    isOk: true,
-  });
-  const [data, setData] = useState<WithOkAndErrorMsg<string>>({
-    inner: '',
-    isOk: true,
-  });
-  const [topics, setTopics] = useState<WithOkAndErrorMsg<string>[] | undefined>(
-    [
-      { inner: '', isOk: true },
-      { inner: '', isOk: true },
-      { inner: '', isOk: true },
-      { inner: '', isOk: true },
-    ],
+  const [rawAbi, setRawAbi] = useState<WithError<string>>({ value: '' });
+  const [data, setData] = useState<WithError<string>>({ value: '' });
+  const [topics, setTopics] = useState<WithError<string>[]>([
+    { value: '' },
+    { value: '' },
+    { value: '' },
+    { value: '' },
+  ]);
+  const [decodeResults, setDecodeResults] = useState<DecodedEventResult[]>([]);
+
+  const signatureHash = useMemo(() => topics.length > 0 && topics[0], [topics]);
+
+  const decodeIsPossible =
+    topics[0].value &&
+    topics.every((topic) => !topic.error) &&
+    data.value &&
+    !data.error;
+
+  const decodeWithAbiIsPossible =
+    decodeIsPossible && rawAbi.value && !rawAbi.error;
+
+  const decodeButtonDisabled = !(
+    !data.error &&
+    data.value &&
+    topics.every(({ error }) => !error) &&
+    (rawAbi.value || tab === '4-bytes')
   );
 
-  const [decodeResults, setDecodeResults] = useState<DecodedEventResult[]>();
+  const decodeResultsDisabled = !error && decodeResults.length === 0;
 
-  const signatureHash = useMemo(
-    () => topics && topics.length > 0 && topics[0],
-    [topics],
-  );
+  const flushResults = (): void => {
+    if (decodeResults.length > 0) setDecodeResults([]);
+  };
 
-  function handleChangeTopic({
+  const setTopic = ({
     index,
-    event,
-    isPasted,
-  }: HandleChangeTopic | HandlePasteTopic): void {
-    if (!topics) return;
-    let value: string;
-    if (isPasted) {
-      value = event.clipboardData.getData('text/plain');
-    } else {
-      value = event.target.value;
-    }
-
-    const parseResult = hexSchema.safeParse(value);
-
-    const stateBeforeIndex = (
-      state: WithOkAndErrorMsg<string>[],
-    ): WithOkAndErrorMsg<string>[] => {
-      return [...state.slice(0, index)];
-    };
-
-    const stateAfterIndex = (
-      state: WithOkAndErrorMsg<string>[],
-    ): WithOkAndErrorMsg<string>[] => {
-      return [...state.slice(index + 1)];
-    };
-
+    ...props
+  }: {
+    index: number;
+    value?: string;
+    error?: string;
+  }): void => {
     setTopics((state) => {
-      if (!state) return;
       return [
-        ...stateBeforeIndex(state),
+        ...stateBeforeIndex(state, index),
         {
           ...state[index],
-          inner: value,
+          ...props,
         },
-        ...stateAfterIndex(state),
+        ...stateAfterIndex(state, index),
       ];
     });
+  };
 
-    if (parseResult.success) {
-      setTopics((state) => {
-        if (!state) return;
-        return [
-          ...stateBeforeIndex(state),
-          {
-            // Cast to the required and boolean isOk type to avoid type errors
-            // regarding the presence of the inner property
-            ...(
-              state as WithOkAndErrorMsg<string> &
-                {
-                  inner: string;
-                  isOk: boolean;
-                }[]
-            )[index],
-            isOk: true,
-            errorMsg: undefined,
-          },
-          ...stateAfterIndex(state),
-        ];
-      });
-    } else {
-      setTopics((state) => {
-        if (!state) return;
-        return [
-          ...stateBeforeIndex(state),
-          {
-            ...state[index],
-            isOk: false,
-            errorMsg: zodResultMessage(parseResult),
-          },
-          ...stateAfterIndex(state),
-        ];
-      });
-    }
-
-    if (value.length === 0) {
-      setTopics((state) => {
-        if (!state) return;
-        return [
-          ...stateBeforeIndex(state),
-          {
-            ...state[index],
-            inner: value,
-            isOk: true,
-            errorMsg: undefined,
-          },
-          ...stateAfterIndex(state),
-        ];
-      });
-    }
-  }
-
-  function handleChangeData(event: ChangeEvent<HTMLInputElement>): void {
-    // clear decode results if something has changed
-    if (decodeResults?.length! > 0) {
-      // @ts-ignore - this is a valid state change
-      setDecodeResults(undefined);
-    }
-
-    let { value } = event.target;
-    value = addHexPrefix(value);
-    const parseResult = hexSchema.safeParse(value);
-    setData({ inner: value, isOk: true });
-
-    if (parseResult.success) {
-      // @ts-ignore - this is a valid state change
-      setData({ inner: value, isOk: true, errorMsg: undefined });
-    } else {
-      setData({
-        isOk: false,
-        errorMsg: zodResultMessage(parseResult),
-      });
-    }
-
-    if (value.length === 0) {
-      // @ts-ignore - this is a valid state change
-      setData({ inner: value, isOk: true, errorMsg: undefined });
-    }
-  }
-
-  function handleChangeRawAbi(event: ChangeEvent<HTMLTextAreaElement>): void {
-    // clear decode results if something has changed
-    if (decodeResults?.length! > 0) {
-      // @ts-ignore - this is a valid state change
-      setDecodeResults(undefined);
-    }
-
-    const { value } = event.target;
-
-    setRawAbi(() => {
-      return { inner: value, isOk: true };
+  const handleChangeTopic = (index: number, newValue: string): void =>
+    handleChangeValidated({
+      newValue,
+      validateFn: (newValue) => hexValidator(newValue),
+      setState: setTopic,
+      flushFn: flushResults,
+      index,
     });
-    // we're currently able to use three abi formats
-    // test if the interface is being created correctly from rawAbi
-    try {
-      parseAbi(value); // throws error if rawAbi format is not valid
-      setRawAbi((state) => {
-        return { ...state, isOk: true };
-      });
-    } catch (error) {
-      setRawAbi((state) => {
-        return {
-          ...state,
-          isOk: false,
-          errorMsg: parseEthersErrorMessage((error as Error).message),
-        };
-      });
-    }
 
-    if (value.length === 0) {
-      setRawAbi((state) => {
-        return {
-          ...state,
-          inner: value,
-          isOk: true,
-          errorMsg: undefined,
-        };
-      });
-    }
-  }
+  const handleChangeData = (newValue: string): void =>
+    handleChangeValidated({
+      newValue,
+      validateFn: (newValue) => hexValidator(newValue),
+      setState: setData,
+      flushFn: flushResults,
+    });
+
+  const handleChangeRawAbi = (newValue: string): void =>
+    handleChangeValidated({
+      newValue,
+      validateFn: (newValue) => abiValidator(newValue),
+      setState: setRawAbi,
+      flushFn: flushResults,
+    });
 
   async function handleDecodeCalldata(): Promise<void> {
-    // @ts-ignore - this is a valid state change
-    setDecodeResults(undefined);
-    if (!signatureHash) {
-      setError('Signature hash is wrong or undefined');
-      return;
-    }
-    if (tab === '4-bytes') {
-      setLoading(true);
-      let decodeResults: DecodedEventResult[] | undefined;
-      try {
-        if (topics && data.isOk && data.inner) {
-          const eventProps: EventProps = {
-            data: data.inner,
-            topics: topics
-              .filter((topic) => {
-                if (topic.isOk) {
-                  return topic.inner.trim().length > 0;
-                }
-                return false;
-              })
-              // we're sure that all of the topics exist because of the filter above
-              .map((topic) => (topic as { inner: string }).inner),
-          };
-          if (signatureHash.isOk) {
-            decodeResults = await decodeWithEventProps(
-              signatureHash.inner,
-              eventProps,
-            );
-          }
-        }
-      } catch (e) {
-        setError(
-          "Provided event couldn't be decoded, maybe there are too few topics?",
-        );
-      } finally {
-        setLoading(false);
-      }
+    setError(undefined);
+    if (tab === '4-bytes' && decodeIsPossible)
+      return void handleDecodeEventWith4Bytes();
+    else if (tab === 'abi' && decodeWithAbiIsPossible)
+      return handleDecodeEventWithAbi();
+  }
 
-      if (decodeResults) {
-        setError('');
-        setDecodeResults(decodeResults);
-      }
-    }
-
-    let decodeResult: DecodedEventResult | undefined;
+  async function handleDecodeEventWith4Bytes(): Promise<void> {
+    setLoading(true);
     try {
-      if (!rawAbi.isOk) return;
-      let abi: Interface | Error;
-      if (rawAbi.inner) {
-        abi = parseAbi(rawAbi.inner);
-      }
-      if (!(abi! instanceof Interface) || !data.isOk || !topics) return;
       const eventProps: EventProps = {
-        data: data.inner,
+        data: data.value,
         topics: topics
           .filter((topic) => {
-            if (topic.isOk) {
-              return topic.inner.trim().length > 0;
-            }
-            return false;
+            return topic.value.trim().length > 0;
           })
-          // we're sure that all of the topics exist because of the filter above
-          .map((topic) => (topic as { inner: string }).inner),
+          .map((topic) => topic.value),
       };
-      decodeResult = decodeEvent(abi, eventProps);
+      const decodeResults = await decodeWithEventProps(
+        (signatureHash as WithError<string>).value,
+        eventProps,
+      );
+      if (decodeResults) {
+        setError(undefined);
+        setDecodeResults(decodeResults);
+      }
     } catch (e) {
       setError(
         "Provided event couldn't be decoded, maybe there are too few topics?",
       );
+    } finally {
+      setLoading(false);
     }
-
-    if (!decodeResult) return;
-    setDecodeResults([decodeResult]);
   }
 
-  const decodeButtonDisabled = !(
-    data.isOk &&
-    topics?.every(({ isOk }) => isOk) &&
-    data.inner &&
-    ((rawAbi.isOk && rawAbi.inner) || tab === '4-bytes')
-  );
+  function handleDecodeEventWithAbi(): void {
+    const abi = parseAbi(rawAbi.value);
 
-  const decodeResultsDisabled =
-    !error && (!decodeResults || decodeResults.length === 0);
+    if (abi instanceof Error)
+      return setRawAbi({
+        ...rawAbi,
+        error: parseEthersErrorMessage(abi.message),
+      });
+    else if (abi instanceof Interface) {
+      const eventProps: EventProps = {
+        data: data.value,
+        topics: topics
+          .filter((topic) => {
+            return topic.value.trim().length > 0;
+          })
+          .map((topic) => topic.value),
+      };
+
+      const decodeResult = decodeEvent(abi, eventProps);
+      setDecodeResults([decodeResult]);
+    }
+  }
 
   return (
     <ToolContainer>
@@ -328,44 +195,44 @@ export default function EventDecoder(): ReactElement {
       />
       <>
         <section className="mb-4">
-          {topics &&
-            topics.map((_, index) => (
-              <section className="mb-2 flex flex-1 flex-col" key={index}>
-                <CopyableConversionInput
-                  name={`topic${index}`}
-                  id={`${index}`}
-                  type="text"
-                  placeholder="0x0.."
-                  // @ts-ignore
-                  error={!topics[index].isOk && topics[index].errorMsg}
-                  className={`${
-                    topics[index].isOk ? 'border-gray-600' : 'border-error/75'
-                  }`}
-                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                    handleChangeTopic({ index, event, isPasted: false })
+          {topics.map((_, index) => (
+            <section className="mb-2 flex flex-1 flex-col" key={index}>
+              <CopyableConversionInput
+                name={`topic${index}`}
+                id={`${index}`}
+                type="text"
+                placeholder="0x0.."
+                error={topics[index].error}
+                className={`${
+                  !topics[index].error ? 'border-gray-600' : 'border-error/75'
+                }`}
+                onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                  handleChangeTopic(index, event.target.value)
+                }
+                onPaste={async (event: ClipboardEvent<HTMLInputElement>) => {
+                  handleChangeTopic(
+                    index,
+                    event.clipboardData.getData('text/plain'),
+                  );
+                  if (index !== 0) return;
+                  const topicValue = event.clipboardData.getData('Text');
+                  const sigHash = topicValue;
+                  if (sigHash) {
+                    await fetch4BytesBy.EventSignatures(sigHash);
                   }
-                  onPaste={async (event: ClipboardEvent<HTMLInputElement>) => {
-                    handleChangeTopic({ index, event, isPasted: true });
-                    if (index !== 0) return;
-                    const topicValue = event.clipboardData.getData('Text');
-                    const sigHash = topicValue;
-                    if (sigHash) {
-                      await fetch4BytesBy.EventSignatures(sigHash);
-                    }
-                  }}
-                />
-              </section>
-            ))}
+                }}
+              />
+            </section>
+          ))}
         </section>
         <CopyableConversionInput
           name="data"
           type="text"
           placeholder="0x0.."
-          // @ts-ignore
-          error={!data.isOk && data.errorMsg}
-          className={`${data.isOk ? 'border-gray-600' : 'border-error/75'}`}
+          error={data.error}
+          className={`${!data.error ? 'border-gray-600' : 'border-error/75'}`}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            handleChangeData(event)
+            handleChangeData(event.target.value)
           }
         />
       </>
@@ -374,7 +241,7 @@ export default function EventDecoder(): ReactElement {
         <AbiSourceTabs
           rawAbi={rawAbi}
           setDecodeResults={setDecodeResults}
-          handleChangeRawAbi={handleChangeRawAbi}
+          handleChangeRawAbi={(event) => handleChangeRawAbi(event.target.value)}
           tabState={{ tab, setTab }}
         />
       </div>
@@ -393,8 +260,8 @@ export default function EventDecoder(): ReactElement {
       </Button>
 
       <section className="pt-8 pb-3">
-        {decodeResults ? (
-          tab === '4-bytes' && decodeResults.length > 0 ? (
+        {decodeResults.length > 0 ? (
+          tab === '4-bytes' ? (
             <p className="text-md pb-4 font-semibold">
               Possible decoded results:
             </p>
@@ -411,15 +278,12 @@ export default function EventDecoder(): ReactElement {
       ) : (
         !decodeResultsDisabled && (
           <section
-            className="relative mb-16 rounded-md border border-gray-600 bg-gray-900 p-8"
+            className="relative mb-16 mt-8 rounded-md border border-gray-600 bg-gray-900 p-8"
             placeholder="Output"
           >
             <section className="flex flex-col gap-4 break-words">
-              {signatureHash && decodeResults?.length! > 0 && (
-                <NodeBlock
-                  className="my-2"
-                  str={(signatureHash.isOk && signatureHash.inner) || '0x0'}
-                >
+              {signatureHash && decodeResults.length > 0 && (
+                <NodeBlock className="my-2" str={signatureHash.value || '0x0'}>
                   <div className="flex items-center gap-2">
                     <p className="truncate">Signature hash</p>
                   </div>
@@ -430,7 +294,7 @@ export default function EventDecoder(): ReactElement {
                 <p className="text-error">{error}</p>
               ) : (
                 <div className="items-left flex flex-col text-ellipsis">
-                  {decodeResults?.map((d, i) => {
+                  {decodeResults.map((d, i) => {
                     return (
                       <section key={i}>
                         <div className="flex flex-col">
