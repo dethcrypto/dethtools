adc24fdab9bba99bd8699862fcb3346327ede844
7sne <kooblatum@gmail.com>
2022-08-17 14:45:49 +0200

    Finish up tests for `vanity-address`

--- src/lib/vanity-address/vanity-address.test.ts
+++ src/lib/vanity-address/vanity-address.test.ts
@@ -1,38 +1,81 @@
-/* eslint-disable no-console */
+import { expect } from 'earljs';
+
 import {
+  formatTime,
   getAddress,
   getPrivateKey,
   getPublicKey,
+  isWallet,
+  replaceZeroAddrAt,
   searchForMatchingWallet,
 } from './vanity-address';
+
+describe(isWallet.name, () => {
+  it('returns true for a valid wallet', () => {
+    expect(isWallet({ address: '', privateKey: '', publicKey: '' })).toEqual(
+      true,
+    );
+  });
+
+  it('returns false for a unvalid wallet', () => {
+    expect(isWallet({ address: '', privateKey: '' })).toEqual(false);
+    expect(isWallet('')).toEqual(false);
+  });
+});
+
+describe(replaceZeroAddrAt.name, () => {
+  it('replaces unchanged chars in address with zeroes', () => {
+    expect(replaceZeroAddrAt('1', '')).toEqual(
+      '1000000000000000000000000000000000000000',
+    );
+    expect(replaceZeroAddrAt('001', '')).toEqual(
+      '0010000000000000000000000000000000000000',
+    );
+    expect(replaceZeroAddrAt('001', '001')).toEqual(
+      '0010000000000000000000000000000000000001',
+    );
+    expect(replaceZeroAddrAt('', '100')).toEqual(
+      '0000000000000000000000000000000000000100',
+    );
+  });
+});
+
+describe(formatTime.name, () => {
+  it('formats time', () => {
+    expect(formatTime(10000)).toEqual('2 hours  46 minutes  40 seconds ');
+    expect(formatTime(5000)).toEqual('1 hours  23 minutes  20 seconds ');
+  });
+});
 
 describe('vanity-address', () => {
   it(getPrivateKey.name, () => {
     const privateKey = getPrivateKey();
-    console.log(privateKey.toString());
+
+    expect(privateKey.length).toEqual(32);
   });
 
   it(getPublicKey.name, () => {
     const privateKey = getPrivateKey();
     const publicKey = getPublicKey(privateKey);
-    console.log(publicKey);
+
+    expect(publicKey.length).toEqual(32);
   });
 
   it(getAddress.name, () => {
     const privateKey = getPrivateKey();
     const publicKey = getPublicKey(privateKey);
     const address = getAddress(publicKey);
-    console.log(address);
+
+    expect(address.length).toEqual(42);
   });
 
-  it(searchForMatchingWallet.name, () => {
+  it(searchForMatchingWallet.name, async () => {
     const config = {
       prefix: 'a',
       suffix: '',
     };
-    const result = searchForMatchingWallet(config);
-    console.log(result);
+    const result = await searchForMatchingWallet(config);
+
+    expect(isWallet(result)).toEqual(true);
   });
 });
-
-export {};
